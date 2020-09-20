7905c9a80835bd80ae2b5c9c7551989032a3b93f
André Aragão <57547336+andrechism@users.noreply.github.com>
2020-09-20 15:43:21 -0300

    Updates

---
+++ precache-manifest.121b52ae1f16a2d5c805297d6fc60f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edfd4ce7f73ff6f81f8f5c1a128ea81c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "807c3a4701bd6fe7f37b",
+    "url": "/monsters-rolodex/static/css/main.15c7d7b4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807c3a4701bd6fe7f37b",
+    "url": "/monsters-rolodex/static/js/main.366f401d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
